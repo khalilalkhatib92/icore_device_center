3c89aec736b810b4156f4393b8042b41c43773b8
Khalil Alkhatib <khalil.alkhatib92@gmail.com>
2022-03-11 20:37:01 +0200

    Marker-shadow.png is SOLVED

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,22 @@ import {MarkdownModule} from'ngx-markdown';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet'
 //import "leaflet/dist/images/marker-shadow.png";
 
+import { icon, Marker } from 'leaflet';
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+Marker.prototype.options.icon = iconDefault;
+
 import {MaterialModule} from './material/material.module';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
